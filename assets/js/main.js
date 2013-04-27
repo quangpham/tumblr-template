0e0c8b840579c6d4f74d10bb5e6df1e793c4ccc2
Quang Pham <me@quangpham.com>
2013-04-27 16:26:48 +0300

    Add active nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,17 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop:0}, 'slow');
         return false;
     });
+		var current_path = window.location.href;
+		console.log(current_path);
+		
+		if (current_path === "http://quangpham.com/") set_menu_active("home");
+		else if (current_path === "http://quangpham.com/about") set_menu_active("about");
+		else if (current_path === "http://quangpham.com/portfolio") set_menu_active("portfolio");
+		else if (current_path === "http://quangpham.com/resume") set_menu_active("resume");
+		else if (current_path === "http://quangpham.com/contact") set_menu_active("contact");
+		
+		function set_menu_active(item) {
+			$("#main-nav li.active").removeClass("active");
+			$("#main-nav li."+item).addClass("active");
+		}
 });
