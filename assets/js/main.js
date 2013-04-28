28c47804ea6812c26b1bfab5a405779b749b37ec
Quang Pham <me@quangpham.com>
2013-04-28 12:06:45 +0300

    tagged pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,10 @@ $(document).ready(function() {
 		var current_path = window.location.href;
 		console.log(current_path);
 		
-		if (current_path === "http://quangpham.com/") set_menu_active("home");
+		if (current_path === "http://quangpham.com/") {
+			set_menu_active("_indexpage");
+			set_page_active("indexpage");
+		}
 		else if (current_path === "http://quangpham.com/about") set_menu_active("about");
 		else if (current_path === "http://quangpham.com/resume") set_menu_active("resume");
 		else if (current_path === "http://quangpham.com/portfolio") set_menu_active("portfolio");
@@ -27,10 +30,28 @@ $(document).ready(function() {
 			or <a target="_blank" href="http://fi.linkedin.com/pub/pham-quang/50/316/72" ><i class="icon-linkedin-sign icon-large" > <b>Linkedin</b></i></a>. </br>\
 			Call me +358 443 460 162 or shoot me an email at me@quangpham.com. <br><br></p>\
 			<p>Quick question? Drop me a line bellow<br>Your question: </p></div>');
+		} 
+		else if (current_path.indexOf("/tagged/") !=-1) {
+			set_menu_active("_blogpage");
+			set_page_active("blogpage");
 		}
 		
+		function set_page_active(page) {
+			$("."+page).show();
+		}
 		function set_menu_active(item) {
 			$("#main-nav li.active").removeClass("active");
 			$("#main-nav li."+item).addClass("active");
 		}
 });
+
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-40478094-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
